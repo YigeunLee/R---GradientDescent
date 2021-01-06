598991067591a7245595fec54d56a90db7f18793
YigeunLee <49982286+YigeunLee@users.noreply.github.com>
2021-01-07 00:59:31 +0900

    Create gradientDescent.R

---
+++ gradientDescent.R
@@ -0,0 +1,24 @@
+library(pracma)
+
+gradient_descent = function(f,x,step = 0.0001){
+  while((f(x) - 8) >= 0.01) {
+    g = grad(f,x); # grad
+    d = -(g / Norm(g)); # direction
+    x = x + (d * step); # update variables
+    
+    d = -grad(f,(x + step * d)) / Norm(grad(f,x + step * d)); # get next direction
+    
+    loss_val = (f(x) - 8)
+    loss <- append(loss, loss_val);
+    print(loss_val);
+  }
+  print('--- optimize x value ---')
+  print(x)
+  print('--- ---------------- ---')
+  return(loss);
+}
+value <- array( c(12,2), dim = c(2) )
+loss_data <- gradient_descent(function(x_vec) x_vec[1] * ( x_vec[2] *  x_vec[2]),value);
+plot(unlist(loss_data))
+
+
